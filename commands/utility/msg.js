bfd4271a70fc7cd0b9448a06f13144861f79c191
adityakashyap7047 <dhruve123456789@gmail.com>
2026-09-03 00:48:39 +0530

    Add hex color support to !msg command

--- commands/utility/msg.js
+++ commands/utility/msg.js
@@ -8,13 +8,26 @@ module.exports = {
     .addStringOption(opt => opt.setName('message').setDescription('Message to send').setRequired(true)),
   cooldown: 3,
   async execute(message, args, client) {
-    const text = args.join(' ');
-    if (!text) {
-      return message.reply({ embeds: [errorEmbed('Error', 'Provide a message.\nUsage: `!msg Hello everyone!`')] });
+    if (!args.length) {
+      return message.reply({ embeds: [errorEmbed('Error', 'Provide a message.\nUsage: `!msg Hello everyone!`\nColor: `!msg #ff0000 Hello!`')] });
+    }
+
+    let color = 0x6366f1;
+    let text = args.join(' ');
+
+    const hexMatch = text.match(/^#([0-9a-fA-F]{3,8})\s+/);
+    if (hexMatch) {
+      const hex = hexMatch[1];
+      color = parseInt(hex, 16);
+      text = text.slice(hexMatch[0].length);
+    }
+
+    if (!text.trim()) {
+      return message.reply({ embeds: [errorEmbed('Error', 'Provide a message after the color.')] });
     }
 
     const embed = new EmbedBuilder()
-      .setColor(0x6366f1)
+      .setColor(color)
       .setDescription(text)
       .setAuthor({
         name: message.member.displayName,
