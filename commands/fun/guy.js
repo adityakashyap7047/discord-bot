62a11c3666db03d8baefdd109618cc114e892a6a
adityakashyap7047 <dhruve123456789@gmail.com>
2026-09-03 10:05:11 +0530

    Use local image for guy command and nitro prank

--- commands/fun/guy.js
+++ commands/fun/guy.js
@@ -1,14 +1,14 @@
-const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
-
-const GUY_IMAGE_URL = 'https://i.imgur.com/PLACEHOLDER.jpg';
+const { SlashCommandBuilder, EmbedBuilder, AttachmentBuilder } = require('discord.js');
+const path = require('path');
 
 module.exports = {
   data: new SlashCommandBuilder().setName('guy').setDescription('Guy'),
   cooldown: 3,
   async execute(message, args, client) {
+    const file = new AttachmentBuilder(path.join(__dirname, '..', '..', 'd05cde43af751fc4445a9f4456d74e93.jpg'));
     const embed = new EmbedBuilder()
       .setColor('#ff6600')
-      .setImage(GUY_IMAGE_URL);
-    message.channel.send({ embeds: [embed] });
+      .setImage('attachment://d05cde43af751fc4445a9f4456d74e93.jpg');
+    message.channel.send({ embeds: [embed], files: [file] });
   },
 };
--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,4 +1,5 @@
-const { Events, EmbedBuilder } = require('discord.js');
+const { Events, EmbedBuilder, AttachmentBuilder } = require('discord.js');
+const path = require('path');
 
 module.exports = {
   name: Events.InteractionCreate,
@@ -7,13 +8,14 @@ module.exports = {
     if (!interaction.isButton()) return;
 
     if (interaction.customId.startsWith('nitro_prank_')) {
+      const file = new AttachmentBuilder(path.join(__dirname, '..', 'd05cde43af751fc4445a9f4456d74e93.jpg'));
       const embed = new EmbedBuilder()
         .setColor('#ff0000')
         .setTitle('Here is your Nitro!')
         .setDescription('You really thought you were getting free Nitro? **LOL**')
-        .setImage('https://i.imgur.com/your-image-url.jpg')
-        .setFooter({ text: 'Gottem! 😂' });
-      await interaction.reply({ embeds: [embed], ephemeral: true });
+        .setImage('attachment://d05cde43af751fc4445a9f4456d74e93.jpg')
+        .setFooter({ text: 'Gottem!' });
+      await interaction.reply({ embeds: [embed], files: [file], ephemeral: true });
     }
   },
 };
